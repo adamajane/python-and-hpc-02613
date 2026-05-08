67847f309100a7c86b8b76f172ea77418ec6dfbb
adamajane <adamajane@gmail.com>
2026-05-08 16:43:36 +0200

    Add pandas size W7 + pi scripts W5+6

--- week_5_6_parallelism_part_1_2/pi-job-script/pi-job-script.sh
+++ week_5_6_parallelism_part_1_2/pi-job-script/pi-job-script.sh
@@ -2,11 +2,21 @@
 #BSUB -J pi-job-script
 #BSUB -q hpc
 #BSUB -W 5
-#BSUB -n 1
+#BSUB -n 10
 #BSUB -R "rusage[mem=512MB]"
 #BSUB -R "select[model == XeonE5_2660v3]"
+#BSUB -R "span[hosts=1]"
 #BSUB -o pi-job-script_%J.out
 #BSUB -e pi-job-script_%J.err
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613_2026
+
+echo "=== Serial ==="
+time python -u pi_fully_serial.py
+
+echo "=== Fully parallel ==="
+time python -u pi_fully_parallel.py
+
+echo "=== Chunked parallel ==="
+time python -u pi_chunked_parallel.py
