f5fbc676ad7936c9a85ac2c2421168f5de5db547
adamajane <adamajane@gmail.com>
2026-05-09 23:02:55 +0200

    Add W9 scripts

--- week_9_numba_and_gpu_computing/gpu-job-script/gpu-job-script.sh
+++ week_9_numba_and_gpu_computing/gpu-job-script/gpu-job-script.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 #BSUB -J gpu-job-script
-#BSUB -q hpc
+#BSUB -q c02613
 #BSUB -W 5
 #BSUB -n 1
-#BSUB -R "rusage[mem=512MB]"
-#BSUB -R "select[model == XeonE5_2660v3]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "rusage[mem=2GB]"
+#BSUB -r "span[hosts=1]"
 #BSUB -o gpu-job-script_%J.out
 #BSUB -e gpu-job-script_%J.err
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613_2026
+
+python -u gpu-program.py
---
+++ week_9_numba_and_gpu_computing/gpu-job-script/gpu-program.py
@@ -0,0 +1,33 @@
+from time import perf_counter
+
+import numpy as np
+from numba import cuda
+
+
+@cuda.jit
+def add_kernel(x, y, out):
+    i = cuda.grid(1)
+    if i < x.size:
+        out[i] = x[i] + y[i]
+
+
+def run(x, y):
+    out = np.empty_like(x)
+    tpb = 128
+    bpg = (x.size + tpb - 1) // tpb
+    add_kernel[bpg, tpb](x, y, out)
+    cuda.synchronize()
+    return out
+
+
+if __name__ == "__main__":
+    n = 1_000_000
+    x = np.random.rand(n).astype(np.float32)
+    y = np.random.rand(n).astype(np.float32)
+
+    run(x, y)  # warm-up
+
+    t0 = perf_counter()
+    run(x, y)
+    t1 = perf_counter()
+    print(f"add_kernel(1M floats): {(t1 - t0) * 1000:.3f} ms")
